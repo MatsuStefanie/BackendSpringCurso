b17f5d1568e58c9fb44111d02091a18595a13560
Matsu Stefanie <matsustefanie@gmail.com>
2020-04-01 18:43:02 -0300

    Rest testado

---
+++ src/main/java/com/cristal/stefanie/cursomc/resources/CategoriaResources.java
@@ -0,0 +1,15 @@
+package com.cristal.stefanie.cursomc.resources;
+
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping(value = "/categorias")
+public class CategoriaResources {
+
+    @RequestMapping(method = RequestMethod.GET)
+    public String funcionar(){
+        return "REST está funcionando";
+    }
+}
