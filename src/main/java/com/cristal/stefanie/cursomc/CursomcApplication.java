df573b6305cf0df455614360c29ccaeecf333d08
Matsu Stefanie <matsustefanie@gmail.com>
2020-04-02 14:18:31 -0300

    Mudando a forma de entrada de dados no banco h2 -  operação de instanciação

--- src/main/java/com/cristal/stefanie/cursomc/CursomcApplication.java
+++ src/main/java/com/cristal/stefanie/cursomc/CursomcApplication.java
@@ -1,12 +1,29 @@
 package com.cristal.stefanie.cursomc;
 
+import com.cristal.stefanie.cursomc.domain.Categoria;
+import com.cristal.stefanie.cursomc.repositores.CategoriaRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
+import java.util.Arrays;
+
 @SpringBootApplication
-public class CursomcApplication {
+public class CursomcApplication implements CommandLineRunner {
+
+    @Autowired
+    private CategoriaRepository categoriaRepository;
 
     public static void main(String[] args) {
         SpringApplication.run(CursomcApplication.class, args);
+    }
+
+    @Override
+    public void run(String... args) throws Exception {
+        Categoria categoria1 = new Categoria(null, "Informática");
+        Categoria categoria2 = new Categoria(null,"Escritório");
+
+        categoriaRepository.saveAll(Arrays.asList(categoria1,categoria2));
     }
 }
