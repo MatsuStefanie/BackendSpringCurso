c11d0af46322fbd227088b3837ea8db3b851aafc
Stefanie Matsu <matsustefanie@gmail.com>
2020-04-07 13:41:19 -0300

    CRUD. Nova categoria pelo metodo POSt

--- src/main/java/com/cristal/stefanie/cursomc/resources/CategoriaResources.java
+++ src/main/java/com/cristal/stefanie/cursomc/resources/CategoriaResources.java
@@ -5,11 +5,10 @@ import com.cristal.stefanie.cursomc.services.CategoriaService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.ApplicationContext;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RequestMethod;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
+import org.springframework.web.servlet.support.ServletUriComponentsBuilder;
 
+import java.net.URI;
 import java.util.ArrayList;
 import java.util.List;
 
@@ -27,4 +26,10 @@ public class CategoriaResources {
         return ResponseEntity.ok().body(obj);
     }
 
+    @RequestMapping(method = RequestMethod.POST)
+    public ResponseEntity<Void> insert(@RequestBody Categoria obj) {
+        obj = service.insert(obj);
+        URI uri = ServletUriComponentsBuilder.fromCurrentRequest().path("/{id}").buildAndExpand(obj.getId()).toUri();
+        return ResponseEntity.created(uri).build();
+    }
 }
