5b327305340d0bc7c3c293cce3cc460703cdc6e5
Matsu Stefanie <matsustefanie@gmail.com>
2020-04-15 15:50:24 -0300

    ~Por necessidade de teste(2) para o heroku indo a versão 8 do java e melhorias em alguns codigos~

--- src/main/java/com/cristal/stefanie/cursomc/resources/utils/URL.java
+++ src/main/java/com/cristal/stefanie/cursomc/resources/utils/URL.java
@@ -10,14 +10,14 @@ import java.util.List;
 import java.util.stream.Collectors;
 
 public class URL {
-    public static List<Integer> decodeIntList(String s) { //decodificar para lista de inteiros
-        List<String> vet = Arrays.stream(s.trim().split(",")).filter(s1 -> ! s1.isBlank()).collect(Collectors.toList());
-        List<Integer> list = new ArrayList<>();
-        for (int i = 0; i < vet.size(); i++) {
-            list.add(Integer.parseInt(vet.get(i)));
-        }
-        return list;
-    }
+//    public static List<Integer> decodeIntList(String s) { //decodificar para lista de inteiros
+//        List<String> vet = Arrays.stream(s.trim().split(",")).filter(s1 -> ! s1.isBlank()).collect(Collectors.toList());
+//        List<Integer> list = new ArrayList<>();
+//        for (int i = 0; i < vet.size(); i++) {
+//            list.add(Integer.parseInt(vet.get(i)));
+//        }
+//        return list;
+//    }
 
     public static String decodeParam(String s) {
         try {
