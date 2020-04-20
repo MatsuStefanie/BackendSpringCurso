20e643a2f5ff0c5110f0447da528e832ca018796
Matsu Stefanie <matsustefanie@gmail.com>
2020-04-20 15:16:29 -0300

    JWT: Configuração inicial do Spring Security. Ajustes no Mock email.

--- src/main/java/com/cristal/stefanie/cursomc/config/TestConfig.java
+++ src/main/java/com/cristal/stefanie/cursomc/config/TestConfig.java
@@ -4,6 +4,7 @@ import com.cristal.stefanie.cursomc.services.DBService;
 import com.cristal.stefanie.cursomc.services.EmailService;
 import com.cristal.stefanie.cursomc.services.MockEmailService;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.context.annotation.Profile;
@@ -17,9 +18,14 @@ public class TestConfig {
     @Autowired
     private DBService dbService;
 
+    @Value("${cristal.gym.control.start-db}")
+    private Boolean iniciarBanco;
+
     @Bean
     public boolean instatiateDatabase() throws ParseException {
-        dbService.instantiateTestDatabase();
+        if (Boolean.FALSE.equals(iniciarBanco)) {
+            return false;
+        }dbService.instantiateTestDatabase();
         return true;
     }
     @Bean
--- src/main/java/com/cristal/stefanie/cursomc/services/SmtpEmailService.java
+++ src/main/java/com/cristal/stefanie/cursomc/services/SmtpEmailService.java
@@ -3,6 +3,7 @@ package com.cristal.stefanie.cursomc.services;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.context.annotation.Profile;
 import org.springframework.mail.MailSender;
 import org.springframework.mail.SimpleMailMessage;
 import org.springframework.mail.javamail.JavaMailSender;
