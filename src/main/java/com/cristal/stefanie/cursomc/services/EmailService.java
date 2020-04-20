3bd185fdebef3e318ca297910d8709ee8fcbe4e0
Matsu Stefanie <matsustefanie@gmail.com>
2020-04-20 12:34:11 -0300

    SERVIÇO DE EMAIL: Email HTML

--- src/main/java/com/cristal/stefanie/cursomc/services/EmailService.java
+++ src/main/java/com/cristal/stefanie/cursomc/services/EmailService.java
@@ -3,7 +3,12 @@ package com.cristal.stefanie.cursomc.services;
 import com.cristal.stefanie.cursomc.domain.Pedido;
 import org.springframework.mail.SimpleMailMessage;
 
+import javax.mail.internet.MimeMessage;
+
 public interface EmailService {
+
+    void sendOrderConfirmationHtmlEmail(Pedido obj);
+    void sendHtmlEmail(MimeMessage msg);
 
     void sendOrderConfirmationEmail(Pedido obj);
     void sendEmail(SimpleMailMessage msg);
--- src/main/java/com/cristal/stefanie/cursomc/services/SmtpEmailService.java
+++ src/main/java/com/cristal/stefanie/cursomc/services/SmtpEmailService.java
@@ -5,6 +5,9 @@ import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.mail.MailSender;
 import org.springframework.mail.SimpleMailMessage;
+import org.springframework.mail.javamail.JavaMailSender;
+
+import javax.mail.internet.MimeMessage;
 
 public class SmtpEmailService extends AbstractEmailService {
 
@@ -12,6 +15,16 @@ public class SmtpEmailService extends AbstractEmailService {
 
     @Autowired
     public MailSender mailSender;
+
+    @Autowired
+    public JavaMailSender javaMailSender;
+
+    @Override
+    public void sendHtmlEmail(MimeMessage msg) {
+        LOG.info("Enviando email...");
+        javaMailSender.send(msg);
+        LOG.info("Email enviado!!!");
+    }
 
     @Override
     public void sendEmail(SimpleMailMessage msg) {
