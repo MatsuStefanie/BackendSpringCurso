8076c08d91115bace59eebe6ca2eb3157b62f011
Matsu Stefanie <matsustefanie@gmail.com>
2020-04-24 15:41:33 -0300

    JWT: Implementando autorização

--- src/main/java/com/cristal/stefanie/cursomc/security/JWTUtil.java
+++ src/main/java/com/cristal/stefanie/cursomc/security/JWTUtil.java
@@ -1,10 +1,12 @@
 package com.cristal.stefanie.cursomc.security;
 
 import io.jsonwebtoken.Jwts;
+import io.jsonwebtoken.Claims;
 import io.jsonwebtoken.SignatureAlgorithm;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.stereotype.Component;
 
+import java.time.Instant;
 import java.time.LocalDateTime;
 import java.time.ZoneOffset;
 import java.util.Date;
@@ -22,8 +24,38 @@ public class JWTUtil {
 
         return Jwts.builder()
                 .setSubject(username)
-                .setExpiration(Date.from(LocalDateTime.now().plusSeconds(expiration).toInstant(ZoneOffset.UTC)))
+                .setExpiration(new Date(System.currentTimeMillis() + expiration))
                 .signWith(SignatureAlgorithm.HS512, secret.getBytes())
                 .compact();
+    }
+
+
+    public boolean tokenValido(String token) {
+        Claims claims = getClaims(token);
+        if (claims != null) {
+            String username = claims.getSubject();
+            Date expirationDate = claims.getExpiration();
+            Date now = new Date(System.currentTimeMillis());
+            if (username != null && expirationDate != null && now.before(expirationDate)) {
+                return true;
+            }
+        }
+        return false;
+    }
+
+    private Claims getClaims(String token) {
+        try {
+            return Jwts.parser().setSigningKey(secret.getBytes()).parseClaimsJws(token).getBody();
+        } catch (Exception e) {
+            return null;
+        }
+    }
+
+    public String getUsername(String token) {
+        Claims claims = getClaims(token);
+        if (claims != null) {
+            return claims.getSubject();
+        }
+        return null;
     }
 }
